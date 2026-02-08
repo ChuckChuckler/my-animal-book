2f9234c164ba87110fc6d7aa69c85381f47028de
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-08 18:50:56 -0500

    Work on reverse/normal geocoding

--- biodiversity-app/src/routes/home/+page.svelte
+++ biodiversity-app/src/routes/home/+page.svelte
@@ -6,15 +6,35 @@
     async function findAnimals(){
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(async (pos)=>{
-                let geoapifyKey = GEOAPIFY_KEY;
-                let geoapifyUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&apiKey=${geoapifyKey}`;
-                await axios.get(geoapifyUrl)
+                let geoapifyKey:string = geoapifyApiKey;
+                let lat:number = pos.coords.latitude;
+                let lon:number = pos.coords.longitude;
+                let geoapifyReverse:string = `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${geoapifyKey}`;
+
+                let city:string = "";
+                let postcode:string = "";
+
+                await axios.get(geoapifyReverse)
                 .then((response)=>{
-                    location = response.data.features[0].properties.city;
+                    city = response.data.features[0].properties.city;
+                    postcode = response.data.features[0].properties.postcode;
+                    location = response.data.features[0].properties.formatted;
                 })
                 .catch((e)=>{
                     location = e;
-                })
+                });
+
+                console.log(city);
+                console.log(postcode);
+                let geoapifyBoundingBox = `https://api.geoapify.com/v1/geocode/search?city=${city}&postcode=${postcode}&apiKey=${geoapifyKey}`;
+                await axios.get(geoapifyBoundingBox)
+                .then((response)=>{
+                    let bounds:number[] = response.data.features[0].bbox;
+                    let latBounds:string = `${bounds[1]},${bounds[3]}`;
+                    let lonBounds:string = `${bounds[0]},${bounds[2]}`;
+                    console.log(latBounds);
+                    console.log(lonBounds);
+                });
             });
         }
     }
