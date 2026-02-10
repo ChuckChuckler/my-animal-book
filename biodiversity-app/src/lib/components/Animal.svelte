8e38c41f711d6a1995ee76ed4cd061a34a9a1457
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-09 23:04:46 -0500

    Successfully retrieving threats/conservation actions from iucn red list api

---
+++ biodiversity-app/src/lib/components/Animal.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import axios from "axios";
+    let { commonName, scientificName, threatLevel} = $props();
+</script>
+
+<div>
+    <h1>{commonName}</h1>
+    <h3>{scientificName}</h3>
+    <h2>{threatLevel}</h2>
+</div>
