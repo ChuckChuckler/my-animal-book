c1172929c5129edb7e4b1c2c04d0f3559cacfe8f
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-15 11:43:01 -0500

    Fetching images for species via inaturalist

---
+++ biodiversity-app/src/lib/components/AnimalImage.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let { imgSrc, imgAlt, attribution } = $props();
+</script>
+
+<img src={imgSrc} alt={imgAlt}>
+<p>{attribution}</p>
--- biodiversity-app/src/routes/animal/[slug]/+page.svelte
+++ biodiversity-app/src/routes/animal/[slug]/+page.svelte
@@ -5,6 +5,8 @@
     import axios from "axios";
     import { goto } from "$app/navigation";
 
+    import AnimalImage from "$lib/components/AnimalImage.svelte";
+
     let { data }:PageProps = $props();
 
     let scientificName = $state("");
@@ -32,6 +34,13 @@
     let threatsDict: Record<string, Threat> = {};
     let threatsArr:Threat[] = $state([]);
 
+    type Img = {
+        imgSrc: string,
+        imgAlt: string,
+        attribution: string
+    }
+    let imgsArr:Img[] = $state([]);
+
     onMount(async ()=>{
         const stored=localStorage.getItem("animalDict");
         if(stored!=null){
@@ -44,6 +53,7 @@
                     assessmentId: animalInfo.mostRecentAssessment
                 }));
                 getInfoAbout(animalInfo.commonName);
+                getImages(animalInfo.scientificName);
                 let threats = response.data.msg.threats;
                 console.log(threats);
                 parseThreats(threats);
@@ -56,13 +66,24 @@
 
     function parseThreats(assessedThreats:any[]):void{
         assessedThreats.forEach(i=>{
-            if(!i.severity.toLowerCase().includes("unknown") && !i.description.en.toLowerCase().includes("unknown") && !i.score.toLowerCase().includes("unknown")){
-                threatsDict[i.description.en] = {
-                    threatName: i.description.en,
-                    score: i.score,
-                    severity: i.severity
+            if(i.severity!=undefined && i.score!=undefined){
+                if(!i.severity.toLowerCase().includes("unknown") && !i.description.en.toLowerCase().includes("unknown") && !i.score.toLowerCase().includes("unknown")){
+                    threatsDict[i.description.en] = {
+                        threatName: i.description.en,
+                        score: i.score,
+                        severity: i.severity
+                    }
+                    threatsArr.push(threatsDict[i.description.en]);
                 }
-                threatsArr.push(threatsDict[i.description.en]);
+            }else{
+                if(!i.description.en.toLowerCase().includes("unknown")){
+                    threatsDict[i.description.en] = {
+                        threatName: i.description.en,
+                        score: i.score,
+                        severity: i.severity
+                    }
+                    threatsArr.push(threatsDict[i.description.en]);
+                }
             }
         })
     }
@@ -79,6 +100,22 @@
             wikiRecord[i.split("==")[0].trim()]=i.split("==")[1].replaceAll("\n","");
         });
     }
+    
+    async function getImages(scientificName:string){
+        try{
+            const response = (await axios.get(`https://api.inaturalist.org/v1/taxa?q=${scientificName.split(" ").join("%20")}`)).data;
+            for(let i:number = 0; i < 5; i++){
+                imgsArr.push({
+                    imgSrc: response.results[i].default_photo.medium_url,
+                    imgAlt: scientificName,
+                    attribution: response.results[i].default_photo.attribution.replaceAll("uploaded by", "uploaded to INaturalist by")
+                })
+                console.log(imgsArr);
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
 
     function rdrctHome(){
         goto("../home");
@@ -91,12 +128,18 @@
 <h3>{scientificName}</h3>
 <h3>{endangeredStatus}</h3>
 <br>
+<div>
+    {#each imgsArr as animalImg}
+        <AnimalImage imgSrc={animalImg.imgSrc} imgAlt={animalImg.imgAlt} attribution={animalImg.attribution}></AnimalImage>
+    {/each}
+</div>
+<br>
 <h2>About</h2>
 <p>{animalAbout}</p>
 <br>
 <h2>Threats</h2>
 <ul>
     {#each threatsArr as threatElement}
-        <li>{`${threatElement.threatName} - ${threatElement.severity}`}</li>
+        <li>{`${threatElement.threatName}`}</li>
     {/each}
 </ul>
