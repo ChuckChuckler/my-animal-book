d413313cf7ca312328fbe2f73d1b067ccf083ca7
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-25 20:50:07 -0500

    more yummy anims to endangered animal displays on homepage

--- biodiversity-app/src/lib/components/Animal.svelte
+++ biodiversity-app/src/lib/components/Animal.svelte
@@ -11,7 +11,7 @@
     })
 </script>
 
-<div id={id} class="bg-[#FAF1DA] hover:bg-[#F4E8C9] flex justify-between rounded-[15px]">
+<div id={id} class="animal-div bg-[#FAF1DA] hover:bg-[#F4E8C9] flex justify-between rounded-[15px]">
     <img src={imgSrc} alt={scientificName} class="rounded-[15px]">
     <div class="w-[75%] m-auto">
         <h1 class="text-[20px] koho">{commonName}</h1>
@@ -22,5 +22,27 @@
 <style>
     .koho{
         font-family: "KoHo", sans-serif;
+    }
+
+    @keyframes animal-div-1{
+        from{width: 100%; height:100%}
+        to{width:102%;height:102%}
+    }
+
+    @keyframes animal-div-2{
+        to{width: 100%; height:100%}
+        from{width:102%;height:102%}
+    }
+
+    .animal-div{
+        width:100%;
+        height:100%;
+        animation: animal-div-2 0.5s ease-out;
+    }
+
+    .animal-div:hover{
+        width:102%;
+        height:102%;
+        animation: animal-div-1 0.5s ease-out;
     }
 </style>
