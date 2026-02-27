4cd3a6e00fde42df2e9e97a6c56f04de185e8c8b
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-26 19:15:57 -0500

    Styling user page

--- biodiversity-app/src/routes/userProfile/[slug]/+page.svelte
+++ biodiversity-app/src/routes/userProfile/[slug]/+page.svelte
@@ -3,8 +3,9 @@
     import type { PageProps } from "./$types";
     import { onMount } from "svelte";
     import { goto } from "$app/navigation";
-
     import AnimalBook from "$lib/components/AnimalBook.svelte";
+
+    import editBtn from "$lib/assets/editBtn.png";
 
     let { data }:PageProps = $props();
 
@@ -118,59 +119,107 @@
     }
     
     async function saveStatus(){
-        if(newStatus!=userStatus){
-            await axios.post("/api/editUserInfo", {
-                username:data.username,
-                pfp: "not changed",
-                bio: "not changed",
-                status: newStatus
-            })
-            .then(()=>{
-                userStatus=newStatus;
-                statusEditor="hidden";
-                statusDisplay="block";
-                statusEditorBtn="block";
-            });
-        }
+        await axios.post("/api/editUserInfo", {
+            username:data.username,
+            pfp: "not changed",
+            bio: "not changed",
+            status: newStatus
+        })
+        .then(()=>{
+            userStatus=newStatus;
+            statusEditor="hidden";
+            statusDisplay="block";
+            statusEditorBtn="block";
+        });
     }
 </script>
 
-<button onclick={home}>Back to home</button>
-
-<div class={`${userDisplay} flex w-[97%] justify-between`}>
-    <div>
-        <div class="flex">
-            <img src={userPfp} alt="user profile" class="w-[200px] rounded-full object-cover aspect-square">
-            <h1>{data.username}</h1>
+<div class="bg-[#E1FFC4] border-box p-[15px]">
+    <button onclick={home} class="koho text-[20px] bg-[#FFDBE6] hover:bg-[#FFC8D7] pink-btn rounded-[18px] hover:bg-[#FFC8D7]">Home</button>
+    <br>
+    <br>
+    <div class="{userDisplay}">
+        <div class="h-[85vh]">
+            <div class="flex justify-start">
+                <img src={userPfp} alt="user profile" class="w-[200px] rounded-full object-cover aspect-square">
+                <div class="mt-auto mb-auto ml-[15px]">
+                    <h1 class="koulen text-[40px]">{data.username}</h1>
+                    <div class="grid grid-cols-2">
+                        <p class="{statusDisplay} w-[100%] koho text-[25px]">{userStatus}</p>
+                        <button class="{statusEditorBtn} ml-[10px]" onclick={editStatus}><img src={editBtn} alt="edit" class="w-[20px] h-[20px]"></button>
+                        <input class="bg-[#FFFAED] pl-[15px] {statusEditor} h-[10vh] rounded-full" type="text" bind:value={newStatus}>
+                        <button class="{statusEditor} koho text-[20px] bg-[#FFDBE6] hover:bg-[#FFC8D7] pink-btn rounded-[18px] hover:bg-[#FFC8D7] m-auto" onclick={saveStatus}>Save</button>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <hr>
+            <br>
+            <p class="koho text-[20px] h-[25vh]">{userBio}</p>
+            <br>
+            <button class="{editVisibility} koho text-[20px] bg-[#FFDBE6] hover:bg-[#FFC8D7] pink-btn rounded-[18px] hover:bg-[#FFC8D7] m-auto " onclick={showEditInterface}>Edit</button>
         </div>
-        <div class="flex w-[20%] justify-between">
-            <p class={`${statusDisplay}`}>{userStatus}</p>
-            <input type="text" bind:value={newStatus} class={`${statusEditor}`}>
-            <button class={`${statusEditorBtn}`} onclick={editStatus}>Edit status</button>
-            <button class={`${statusEditor}`} onclick={saveStatus}>Save</button>
+        <div class="h-[95vh]">
+            <br>
+            <br>
+            <div>
+                <h1 class="text-center text-[40px] koulen">{data.username}'s Animal Book</h1>
+                <div>
+                    <AnimalBook bookOwner={`${data.username}`} ownerIsUser={false} clickable={false}></AnimalBook>
+                </div>
+            </div>
         </div>
-        <br>
-        <p>{userBio}</p>
-        <br>
-        <button class={`${editVisibility}`} onclick={showEditInterface}>Edit</button>
     </div>
-    <div>
-        <AnimalBook bookOwner={`${data.username}`} ownerIsUser={false} width="60" clickable={false}></AnimalBook>
+
+    <div class={`${editingInterface}`}>
+        <h1>{data.username}</h1>
+        <img src={newUserPfp} alt="user profile" class="w-[200px] rounded-full object-cover aspect-square">
+        <label for="editImage">Edit image: </label>
+        <input type="file" accept="image/*" bind:this={newImageInput} id="editImage" onchange={showNewImg}>
+        <br>
+        <br>
+        <label for="editBio">Edit bio:</label>
+        <br>
+        <textarea id="editBio" bind:value={newBio}></textarea>
+        <br>
+        <br>
+        <button onclick={saveEdits}>Save</button>
+        <button onclick={cancelEdits}>Cancel</button>
     </div>
 </div>
 
-<div class={`${editingInterface}`}>
-    <h1>{data.username}</h1>
-    <img src={newUserPfp} alt="user profile" class="w-[200px] rounded-full object-cover aspect-square">
-    <label for="editImage">Edit image: </label>
-    <input type="file" accept="image/*" bind:this={newImageInput} id="editImage" onchange={showNewImg}>
-    <br>
-    <br>
-    <label for="editBio">Edit bio:</label>
-    <br>
-    <textarea id="editBio" bind:value={newBio}></textarea>
-    <br>
-    <br>
-    <button onclick={saveEdits}>Save</button>
-    <button onclick={cancelEdits}>Cancel</button>
-</div>
+<style>
+    .koulen{
+        font-family: "Koulen", sans-serif;
+    }
+
+    .koho{
+        font-family: "koho", sans-serif;
+    }
+
+    .kaisei-tokumin{
+        font-family: "kaisei-tokumin", serif;
+    }
+
+    @keyframes pink-btn-1{
+        from{width: 160px; height: 50px}
+        to{width: 165px; height: 55px}
+    }
+
+    @keyframes pink-btn-2{
+        to{width: 160px; height: 50px}
+        from{width: 165px; height: 55px}
+    }
+
+    .pink-btn{
+        width: 160px;
+        height: 50px;
+        animation: pink-btn-2 0.5s ease-out;
+    }
+
+    .pink-btn:hover{
+        width: 165px;
+        height: 55px;
+        animation: pink-btn-1 0.5s ease-out;
+    }
+</style>
