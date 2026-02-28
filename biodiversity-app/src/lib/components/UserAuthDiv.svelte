ae4725c11e7dd3a92a0227b734d8ad4e4c9c8b50
Avishi Sharma <robloxpusheen8@gmail.com>
2026-02-27 22:44:20 -0500

    Styling user auth page w/ some art yay

--- biodiversity-app/src/lib/components/UserAuthDiv.svelte
+++ biodiversity-app/src/lib/components/UserAuthDiv.svelte
@@ -34,15 +34,79 @@
     }
 </script>
 
-<h3>{authType}</h3>
-<label for="user">Username</label>
-<br>
-<input type="text" placeholder="Username" autocomplete="off" id="user" bind:value={user}>
-<br>
-<label for="pass">Password</label>
-<br>
-<input type="password" placeholder="Password" autocomplete="off" id="pass" bind:value={pass}>
-<br>
-<p>{errMsg}</p>
-<br>
-<button onclick={sendToAuth}>{authType}</button>
+<div class="bg-[#c0f0af] auth-div rounded-[50px] border-box p-[15px] m-auto z-10">
+    <h3 class="text-center text-[40px] koulen">{authType}</h3>
+    <div class="m-auto w-[85%]">
+        <label for="user" class="kaisei-tokumin text-[30px]">Username</label>
+        <br>
+        <input type="text" placeholder="Username" autocomplete="off" id="user" class="bg-[#FFFAED] w-[100%] h-[50px] pl-[10px] rounded-full koho" bind:value={user}>
+    </div>
+    <br>
+    <div class="m-auto w-[85%]">
+        <label for="pass" class="kaisei-tokumin text-[30px]">Password</label>
+        <br>
+        <input type="password" placeholder="Password" autocomplete="off" id="pass" class="bg-[#FFFAED] w-[100%] h-[50px] pl-[10px] rounded-full koho" bind:value={pass}>
+    </div>
+    <br>
+    <p class="text-red-500">{errMsg}</p>
+    <br>
+    <button class="block auth-btn bg-[#FFDBE6] hover:bg-[#FFC8D7] rounded-[23px] m-auto koho text-[25px]" onclick={sendToAuth}>{authType}</button>
+</div>
+
+<style>
+    .koulen{
+        font-family: "Koulen", sans-serif;
+    }
+
+    .kaisei-tokumin{
+        font-family: "Kaisei Tokumin", serif;
+    }
+
+    .koho{
+        font-family: "KoHo", sans-serif;
+    }
+
+    @keyframes nav-btns-1{
+        to{width:52%;height:100px}
+        from{width:50%;height:98px}
+    }
+
+    @keyframes nav-btns-2{
+        from{width:52%;height:100px}
+        to{width:50%;height:98px}
+    }
+
+    .auth-btn{
+        width: 50%;
+        height: 98px;
+        animation: nav-btns-2 0.5s ease-out;
+    }
+
+    .auth-btn:hover{
+        width: 52%;
+        height: 98px;
+        animation: nav-btns-1 0.5s ease-out;
+    }
+
+    @keyframes auth-div-1{
+        from{width:48%;height:85vh;}
+        to{width:50%;height:86vh}
+    }
+
+    @keyframes auth-div-2{
+        to{width:48%;height:85vh;}
+        from{width:50%;height:86vh}
+    }
+
+    .auth-div{
+        width:48%;
+        height:85vh;
+        animation: auth-div-2 0.5s ease-out;
+    }
+
+    .auth-div:hover{
+        width:50%;
+        height:86vh;
+        animation: auth-div-1 0.5s ease-out;
+    }
+</style>
--- biodiversity-app/src/routes/+page.svelte
+++ biodiversity-app/src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang="ts">
     import { onMount } from "svelte";
     import UserAuthDiv from "$lib/components/UserAuthDiv.svelte";
+
+    import animalBookLogo from "$lib/assets/logo.png";
+    import overlayTop from "$lib/assets/overlay_top.png";
+    import overlayBottom from "$lib/assets/overlay_bottom.png";
 
     onMount(()=>{
         const stored=localStorage.getItem("animalDict");
@@ -10,8 +14,35 @@
     })
 </script>
 
-<h1>My Animal Book</h1>
-<UserAuthDiv authType = "Sign Up"></UserAuthDiv>
-<br>
-<br>
-<UserAuthDiv authType = "Log In"></UserAuthDiv>
+<div class="h-[100vh] relative bg-linear-to-b from-[#61cdff] to-[#E2FFF5]">
+    <img src={overlayTop} alt="overlay top" class="object-cover w-[100%]">
+    <img src={animalBookLogo} alt="my animal book logo" class="app-logo absolute left-[50%] top-[50%] transform-[translate(-50%,-50%)] z-3">
+    <img src={overlayBottom} alt="overlay bottom" class="object-cover w-[100%] absolute bottom-0">
+</div>
+
+<div class="flex justify-around w-[100%] bg-[#3f7d28] h-[100vh]">
+    <UserAuthDiv authType = "Sign Up"></UserAuthDiv>
+    <UserAuthDiv authType = "Log In"></UserAuthDiv>
+</div>
+
+<style>
+    @keyframes app-logo-1{
+        from{width:50vw}
+        to{width:54vw}
+    }
+
+    @keyframes app-logo-2{
+        to{width:50vw}
+        from{width:54vw}
+    }
+
+    .app-logo{
+        width:50vw;
+        animation: app-logo-2 0.5s ease-out;
+    }
+
+    .app-logo:hover{
+        width:54vw;
+        animation: app-logo-1 0.5s ease-out;
+    }
+</style>
